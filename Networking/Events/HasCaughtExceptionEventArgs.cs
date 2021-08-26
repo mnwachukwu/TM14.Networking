e93879945ca4dd05db26b4d447a9af33fc84a69f
Matt N <matt.nwachukwu@gmail.com>
2021-08-26 13:34:22 -0400

    Added missing documentation to new eventargs class

--- Networking/Events/HasCaughtExceptionEventArgs.cs
+++ Networking/Events/HasCaughtExceptionEventArgs.cs
@@ -2,10 +2,19 @@
 
 namespace TM14.Networking.Events
 {
+    /// <summary>
+    /// Arguements to raise with a HasCaughtException event.
+    /// </summary>
     public class HasCaughtExceptionEventArgs : EventArgs
     {
+        /// <summary>
+        /// The exception that was caught.
+        /// </summary>
         public Exception Exception { get; set; }
 
+        /// <summary>
+        /// The time at which the exception was caught.
+        /// </summary>
         public DateTime TimeStamp { get; set; }
     }
 }
